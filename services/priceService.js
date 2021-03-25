b7d072dae758dc05dfe37b632a7fbda3c73d6480
Ndifreke Friday <ndiecodes@gmail.com>
2021-03-25 20:04:20 +0100

    refactor: float to 2 decimal places

--- services/priceService.js
+++ services/priceService.js
@@ -1,43 +1,41 @@
 const { default: axios } = require("axios");
 
 const AllowedTypes = {
-    BUY: "buy",
-    SELL: "sell",
-  };
-  
-  GET_BTC_USD_PRICE = async () => {
-    try {
-      const { data } = await axios.get(
-        "https://api.coindesk.com/v1/bpi/currentprice.json"
-      );
-      return data.bpi.USD.rate_float;
-    } catch (error) {
-      throw error;
-    }
-  };
+  BUY: "buy",
+  SELL: "sell",
+};
 
-  getPurchaseRate = (btcPrice, args) => {
-    let amount;
-    switch (args.type) {
-      case AllowedTypes.BUY:
-        amount = btcPrice + (btcPrice * args.margin);
-        break;
-      case AllowedTypes.SELL:
-        amount = btcPrice - (btcPrice * args.margin);
-        break;
-      default:
-        break;
-    }
-    //convert USD to NGN
-    amount = amount * args.exchangeRate;
-
-    return amount;
+GET_BTC_USD_PRICE = async () => {
+  try {
+    const { data } = await axios.get(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    return data.bpi.USD.rate_float;
+  } catch (error) {
+    throw error;
   }
+};
 
-
-
-  module.exports = {
-    AllowedTypes,
-    GET_BTC_USD_PRICE,
-    getPurchaseRate,
+getPurchaseRate = (btcPrice, args) => {
+  let amount;
+  switch (args.type) {
+    case AllowedTypes.BUY:
+      amount = btcPrice + (btcPrice * args.margin);
+      break;
+    case AllowedTypes.SELL:
+      amount = btcPrice - (btcPrice * args.margin);
+      break;
+    default:
+      break;
   }
+  //convert USD to NGN
+  amount = amount * args.exchangeRate;
+
+  return amount.toFixed(2);
+};
+
+module.exports = {
+  AllowedTypes,
+  GET_BTC_USD_PRICE,
+  getPurchaseRate,
+};
